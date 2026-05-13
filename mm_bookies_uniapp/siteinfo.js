da36313ce46664e298126e21d0a2fd57dd5c1872
Arthur <arthur@jx.cn>
2026-05-13 19:11:56 +0800

    更改配置

--- mm_bookies_uniapp/siteinfo.js
+++ mm_bookies_uniapp/siteinfo.js
@@ -1,25 +1,22 @@
+//正式环境
 var siteinfo = {
 	"tenant_id": "10000",
-	"apiUrl": "http://m.1x2mmm.net/api", //后端接口
-	"imgUrl": "http://m.1x2mmm.net/img", //java后端图片前缀
-	"payUrl": "http://m.okbetmm.com/payapi", //支付中心
-	"wsUrl": "ws://ag.1x2mmm.net", //WebSocket服务器
+	"apiUrl": "http://8.213.214.83:8090/api", //后端接口
+	"imgUrl": "http://8.213.214.83:8090img", //java后端图片前缀中心
+	"payUrl": "http://payapi.1x2mmm.net", //支付中心
+	"wsUrl": "ws://8.213.214.83:8082", //WebSocket服务器
 	"awcImgUrl": "https://tttuat.apihub55.com", //awc图片前缀
 	version: 'v2.0.1'
 };
 
 if (process.env.NODE_ENV == 'development') {
 	//开发
-	//siteinfo.apiUrl = "http://192.168.99.126:8282";
 	// siteinfo.apiUrl = "http://localhost:8282";
 	siteinfo.apiUrl = "http://m.1x2mmm.net/api";
 	siteinfo.imgUrl = "http://m.1x2mmm.net/img"; //java后端图片前缀
-	// siteinfo.wsUrl = "ws://192.168.99.126:8082";//开发环境WebSocket服务器
-	siteinfo.payUrl = "http://192.168.99.125:9010";
-	siteinfo.awcImgUrl = "https://tttuat.apihub55.com"; //awc图片前缀
 
 } else {
-	//正式环境默认
+	//测试环境
 
 }
 
